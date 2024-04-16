29f8c9a8e2f7e421e98226937617767d50c90d68
t_kodama <darmadevdone@gmail.com>
2024-04-16 13:32:56 +0900

    Hello,Worldの表示

--- app/src/main/java/jp/co/yumemi/droidtraining/MainActivity.kt
+++ app/src/main/java/jp/co/yumemi/droidtraining/MainActivity.kt
@@ -1,12 +1,35 @@
 package jp.co.yumemi.droidtraining
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import jp.co.yumemi.droidtraining.ui.theme.AndroidPracticeTheme
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            AndroidPracticeTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background,
+                ) {
+                    Box(
+                        modifier = Modifier.fillMaxSize(),
+                        Alignment.Center
+                    ) {
+                        Text("Hello, World")
+                    }
+                }
+            }
+        }
     }
 }
