734bcc4bbf23d29a212859323328827958c48c44
t_kodama <darmadevdone@gmail.com>
2024-04-19 12:44:52 +0900

    天気を取得する関数

---
+++ app/src/main/java/jp/co/yumemi/droidtraining/presentation/viewmodel/WeatherViewModel.kt
@@ -0,0 +1,20 @@
+package jp.co.yumemi.droidtraining.presentation.viewmodel
+
+import android.content.Context
+import androidx.compose.runtime.mutableStateOf
+import jp.co.yumemi.api.YumemiWeather
+import jp.co.yumemi.droidtraining.presentation.state.WeatherState
+
+class WeatherViewModel {
+    private val _weatherState = mutableStateOf<WeatherState>(WeatherState.Loading)
+    val weatherState = _weatherState
+
+    fun getWeather(context: Context) {
+        try {
+            val response = YumemiWeather(context).fetchThrowsWeather()
+            _weatherState.value = WeatherState.Success(weather = response)
+        } catch (e: Throwable) {
+            _weatherState.value = WeatherState.Error(error = e)
+        }
+    }
+}
