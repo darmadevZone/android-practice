9fd9e4074b9e08af65d1fac731ec2445a08840b7
t_kodama <darmadevdone@gmail.com>
2024-04-19 12:46:16 +0900

    不要なimportの削除

--- app/src/main/java/jp/co/yumemi/droidtraining/MainActivity.kt
+++ app/src/main/java/jp/co/yumemi/droidtraining/MainActivity.kt
@@ -1,14 +1,9 @@
 package jp.co.yumemi.droidtraining
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.BoxWithConstraints
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.width
@@ -21,7 +16,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
