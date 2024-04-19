45953a013e9bde74254636b73f0fb830359c4b18
t_kodama <darmadevdone@gmail.com>
2024-04-19 12:48:05 +0900

    componentの分割

--- app/src/main/java/jp/co/yumemi/droidtraining/MainActivity.kt
+++ app/src/main/java/jp/co/yumemi/droidtraining/MainActivity.kt
@@ -5,25 +5,21 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.width
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.constraintlayout.compose.ConstraintLayout
-import androidx.constraintlayout.compose.Dimension
-import jp.co.yumemi.api.YumemiWeather
-import jp.co.yumemi.droidtraining.presentation.components.RowButtons
-import jp.co.yumemi.droidtraining.presentation.components.Temperature
-import jp.co.yumemi.droidtraining.presentation.constant.Image.ImageConstants
+import jp.co.yumemi.droidtraining.presentation.components.ErrorAlertDialog
+import jp.co.yumemi.droidtraining.presentation.constant.image.ImageConstants
+import jp.co.yumemi.droidtraining.presentation.screen.WeatherScreen
+import jp.co.yumemi.droidtraining.presentation.state.WeatherState
+import jp.co.yumemi.droidtraining.presentation.viewmodel.WeatherViewModel
 import jp.co.yumemi.droidtraining.ui.theme.AndroidPracticeTheme
 
 class MainActivity : ComponentActivity() {
@@ -36,10 +32,38 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background,
                 ) {
-                    Box(
-                        modifier = Modifier.fillMaxSize(),
-                    ) {
-                        SomeLayout()
+                    val viewModel = remember {
+                        WeatherViewModel()
+                    }
+                    val context = LocalContext.current
+                    LaunchedEffect(Unit) {
+                        viewModel.getWeather(context)
+                    }
+                    when (val weatherState = viewModel.weatherState.value) {
+                        is WeatherState.Success -> {
+                            WeatherScreen(
+                                onReloadClick = {
+                                    viewModel.getWeather(context)
+                                },
+                                onNextClick = {},
+                                weather = weatherState.weather
+                            )
+                        }
+
+                        is WeatherState.Error -> {
+                            ErrorAlertDialog(
+                                onCloseClick = {},
+                                onReloadClick = {
+                                    viewModel.getWeather(context)
+                                },
+                            )
+                        }
+
+                        is WeatherState.Loading -> {
+                            Box(contentAlignment = Alignment.Center) {
+                                CircularProgressIndicator()
+                            }
+                        }
                     }
                 }
             }
@@ -47,82 +71,10 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-
-@Composable
-fun SomeLayout() {
-    val context = LocalContext.current
-    var weather by remember {
-        mutableStateOf("")
-    }
-    BoxWithConstraints {
-        val imagesHalfWidth = maxWidth / 2
-
-        ConstraintLayout(modifier = Modifier.fillMaxSize()) {
-            val (images, buttons) = createRefs()
-            Column(modifier = Modifier
-                .width(imagesHalfWidth)
-                .constrainAs(images) {
-                    start.linkTo(parent.start)
-                    end.linkTo(parent.end)
-                    top.linkTo(parent.top)
-                    bottom.linkTo(parent.bottom)
-                }) {
-                WeatherContent(weather = weather)
-                Temperature(modifier = Modifier.fillMaxWidth())
-            }
-            RowButtons(modifier = Modifier.constrainAs(buttons) {
-                val margin = 80.dp
-                top.linkTo(images.bottom, margin = margin)
-                start.linkTo(images.start)
-                end.linkTo(images.end)
-                width = Dimension.fillToConstraints
-            },
-                onNextClick = {/* TODO */ },
-                onReloadClick = {
-                    val response = YumemiWeather(context = context).fetchSimpleWeather()
-                    Log.d("response", response)
-                    weather = response
-                })
-        }
-    }
-}
-
-@Composable
-fun WeatherContent(weather: String) {
-    val weatherInfo = when (weather) {
-        "sunny" -> {
-            ImageConstants.sunny
-        }
-
-        "snow" -> {
-            ImageConstants.snow
-        }
-
-        "rainy" -> {
-            ImageConstants.rainy
-        }
-
-        "cloudy" -> {
-            ImageConstants.cloudy
-        }
-
-        else -> {
-            //defaultでは、sunny
-            ImageConstants.sunny
-        }
-    }
-
-    Image(
-        modifier = Modifier.aspectRatio(1f / 1f),
-        painter = painterResource(weatherInfo),
-        contentDescription = "droid",
-    )
-}
-
 @Preview
 @Composable
 fun SomeLayoutPreview() {
     Surface {
-        SomeLayout()
+        WeatherScreen(onNextClick = {}, onReloadClick = {}, weather = ImageConstants.Sunny.weather)
     }
 }
--- app/src/main/java/jp/co/yumemi/droidtraining/presentation/components/RowButtons.kt
+++ app/src/main/java/jp/co/yumemi/droidtraining/presentation/components/RowButtons.kt
@@ -1,27 +1,47 @@
 package jp.co.yumemi.droidtraining.presentation.components
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.shape.CutCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import jp.co.yumemi.droidtraining.R
 
 @Composable
 fun RowButtons(
-    modifier: Modifier = Modifier, onReloadClick: () -> Unit, onNextClick: () -> Unit
+    modifier: Modifier = Modifier,
+    onReloadClick: () -> Unit,
+    onNextClick: () -> Unit
 ) {
     val reload = stringResource(id = R.string.reload)
     val next = stringResource(id = R.string.next)
-    val context = LocalContext.current
-    Row(modifier = modifier) {
-        Button(modifier = Modifier.weight(1f), onClick = onReloadClick) {
-            Text(text = reload)
+    Row(modifier = modifier, horizontalArrangement = Arrangement.spacedBy(18.dp)) {
+        Button(
+            onClick = onReloadClick,
+            shape = CutCornerShape(5.dp),
+            colors = ButtonDefaults.textButtonColors(
+                containerColor = Color.Black
+            ),
+            modifier = Modifier
+                .weight(1f)
+        ) {
+            Text(text = reload, color = Color.White)
         }
-        Button(modifier = Modifier.weight(1f), onClick = onNextClick) {
-            Text(text = next)
+        Button(
+            onClick = onNextClick,
+            shape = CutCornerShape(5.dp),
+            colors = ButtonDefaults.textButtonColors(
+                containerColor = Color.Black
+            ),
+            modifier = Modifier.weight(1f),
+        ) {
+            Text(text = next, color = Color.White)
         }
     }
 }
