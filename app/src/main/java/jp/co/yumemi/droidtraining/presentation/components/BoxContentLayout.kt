fbd26b22a8f7411963005eb25bed7c7a9801fd8d
t_kodama <darmadevdone@gmail.com>
2024-04-19 16:34:50 +0900

    BoxContentLayoutコンポーネントに分割

---
+++ app/src/main/java/jp/co/yumemi/droidtraining/presentation/components/BoxContentLayout.kt
@@ -0,0 +1,51 @@
+package jp.co.yumemi.droidtraining.presentation.components
+
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
+
+@Composable
+fun BoxContentLayout(
+    weather: String,
+    onNextClick: () -> Unit,
+    onReloadClick: () -> Unit,
+) {
+    BoxWithConstraints {
+        val imagesHalfWidth = maxWidth / 2
+
+        ConstraintLayout(modifier = Modifier.fillMaxSize()) {
+            val (images, buttons) = createRefs()
+            Column(
+                modifier = Modifier
+                    .width(imagesHalfWidth)
+                    .constrainAs(images) {
+                        start.linkTo(parent.start)
+                        end.linkTo(parent.end)
+                        top.linkTo(parent.top)
+                        bottom.linkTo(parent.bottom)
+                    }
+            ) {
+                WeatherImage(weather = weather)
+                Temperature(modifier = Modifier.fillMaxWidth())
+            }
+            RowButtons(
+                modifier = Modifier.constrainAs(buttons) {
+                    val margin = 80.dp
+                    top.linkTo(images.bottom, margin = margin)
+                    start.linkTo(images.start)
+                    end.linkTo(images.end)
+                    width = Dimension.fillToConstraints
+                },
+                onNextClick = onNextClick,
+                onReloadClick = onReloadClick
+            )
+        }
+    }
+}
